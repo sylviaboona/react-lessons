68fe2ec779bd69dddd1a85aed51884dfc2ebe305
ericmuh <muhwezi1000@gmail.com>
2021-05-17 14:33:23 +0300

    updated todoapp

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,18 @@
 import React, { useState } from 'react';
 import './assets/css/App.css';
-import Counter from './components/Counter';
 import Form from './components/Form';
-import LoginForm from './components/Login';
 import TodoList from './components/TodoList';
 
-// const todos = ["hello today", "go shopping", "go for Eid"]
 
-// NOTE ;: HTML_like syntax ==> JSX, others are normal JS/TS
-
-// To pass JS to JSX we use {}
 function App() {
   const [todos, setTodos] = useState(["Go for Eid"])
   return (
     <div className="App">
-      {/* <section>
-        <h1>Todo app</h1>
-        <Form mytodos={todos} setTodos={setTodos} />
-        <TodoList mytodos={todos} />
-      </section>
       <section>
-        <h1>counter</h1>
-        <Counter />
-      </section> */}
-      <section>
-        <h1>Login Form</h1>
-        <LoginForm />
+        <h1>Todo App</h1>
+        <Form />
+        <TodoList />
       </section>
-
 
     </div>
   );
